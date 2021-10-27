e085e498bd6ca1caaf44eff9e53202c02fddf5f2
Technical-Pro <john.srkpm@gmail.com>
2021-10-27 09:06:56 +0530

    setup.sh
    Dear learners ,
         This file contains basic setup for kali linux installed on your system ...
          If anyone found it helpful , please like my repository ....
    Thankyou.
    
    Owner 
    Grey-Hat99 
    john.srkpm@gmail.com

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
+clear
 echo "[*] Starting Basic setup ...."
 sleep 1
 apt install sudo xfce4 perl php apache2 metasploit-framework wifite aircrack-ng apt-utils python3 python3-pip python2 figlet cowsay toilet nano wget curl tigervnc-standalone-server routersploit burpsuite yasm libgmp-dev libpcap-dev libnss3-dev libkrb5-dev pkg-config libbz2-dev zlib1g-dev build-essential libssl-dev sqlmap nikto clang golang php 
-time.sleep 1
+sleep 1
 clear
 echo "[*] Starting apache2 service..."
 service apache2 start
 pip3 install colorama requests pynacl wheel paramiko pyfiglet bs4 keyboard 
 clear 
-echo "[*] Bsic setup complete ..."
+echo "[*] Basic setup complete ..."
 time.sleep 2
 echo "[*] Give my repository a like ..."
 
